54cc57f1017269579bf314a1d11b3248de824222
DENTAGEST\rayan.boiteau <rayan.boiteau@dentagest.ch>
2025-02-05 11:00:34 +0100

    auth first commit

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,6 +1,5 @@
 import { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
 
 
 export const authOptions : NextAuthOptions = {
